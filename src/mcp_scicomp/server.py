e08677b31adf60b194e6054914ecdf0492afeb83
Younes <saddoug.younes@gmail.com>
2026-03-09 05:58:15 +0100

    initial project skeleton, plotting & data utils

---
+++ src/mcp_scicomp/server.py
@@ -0,0 +1,6 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("mcp-scicomp")
+
+# Tool modules will be imported here once implemented
+# from mcp_scicomp.tools import probability, stochastic, ode, pde, sde, optimization, linalg
